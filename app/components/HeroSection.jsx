7db9f054ecd65efe1942128d2b25c71dcdbb385b
Shinji Ishikawa <shinG.ishikawa@gmail.com>
2025-07-14 13:50:29 +0800

    edited image

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -47,7 +47,7 @@ const HeroSection = () =>{
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
           <div className='relative rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] '>
             <Image
-              src="/images/Me.jpg"
+              src="/images/me.jpg"
               alt='hero image'
               className='absolute tranform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
               width={300}
